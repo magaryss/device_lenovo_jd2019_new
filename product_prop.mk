0d4562e2fdf7201250ee41e80bdb3c90e181b120
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-02 16:28:55 -0300

    jd2019: Use pixel fp to bypass safetynet

--- product_prop.mk
+++ product_prop.mk
@@ -26,10 +26,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     media.settings.xml=/system/etc/media_profiles_vendor.xml
 
-# Pixel identification
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.build.fingerprint=google/flame/flame:10/QQ3A.200705.002/6506677:user/release-keys
-
 # Telephony
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
