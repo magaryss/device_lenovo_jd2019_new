DerpLab/device_xiaomi_sdm710-common

sdm710-common: Add a custom lights HAL
Change-Id: Iafa2ba2338932879b7a1dc480b50559e3328fd21

sdm710.mk
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_sdm710
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0

magaryss/device_lenovo_jd2019_new

 * This overrides stock liblights and fixes LCD backlight issues.
 * Also fix lights HAL denials

sdm710.mk
@@ -39,6 +39,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.realme_sdm710
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0