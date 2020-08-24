7e1bbfe3a068e58820ee2de3a3d6beb6a136bb70
merser2005 <merser2005@gmail.com>
2020-08-24 00:10:11 +0000

    jd2019: new dalvik values

--- product_prop.mk
+++ product_prop.mk
@@ -50,3 +50,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Zygote
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+#Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=16m
