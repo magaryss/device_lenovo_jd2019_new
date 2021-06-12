a56358e41ac99fde717a0437dfec8b21644f3746
magaryss <71859385+magaryss@users.noreply.github.com>
2021-06-12 18:59:14 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_jd2019.mk
+    $(LOCAL_DIR)/rr_jd2019.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_jd2019-user \
-    lineage_jd2019-userdebug \
-    lineage_jd2019-eng
+    rr_jd2019-user \
+    rr_jd2019-userdebug \
+    rr_jd2019-eng
